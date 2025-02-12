3368e5f22aa18ec19f0b8864d5f227f32766e56e
charlottte-bl <cboucherie@insa-rouen.fr>
2025-02-12 11:18:10 +0100

    oups pipeline

--- run_pipeline_synthetic.sh
+++ run_pipeline_synthetic.sh
@@ -11,7 +11,7 @@ for i in $(seq 1 $xp); do
 			echo "XP $i :"
 			echo " | Parameters : encoding_dim = $dim"
 			echo " || Dataset : index = $dataset"
-        	python3 pipeline.py --encoding_dim "$dim" --epochs 50 --index "$dataset" --encoding_channel 1 --loss euclidean
+        	python3 pipeline.py --encoding_dim "$dim" --epochs 50 --index "$dataset" --encoding_channel 1 --loss euclid
 		done
     done
 done
