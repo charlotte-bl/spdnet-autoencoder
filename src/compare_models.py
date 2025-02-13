6258151a86661eed0b841f0f32b920519862cc1f
charlottte-bl <cboucherie@insa-rouen.fr>
2025-02-13 15:24:10 +0100

    comments compare

--- src/compare_models.py
+++ src/compare_models.py
@@ -5,6 +5,7 @@ from visualization import show_metrics_from_dicts
 import os
 
 def load_dict(path,name):
+    # load a dictionnary
     data = np.load(path+name)
     loaded_keys = data['keys']
     loaded_means = data['means']
@@ -13,21 +14,24 @@ def load_dict(path,name):
     return reconstructed_dict
 
 def compare_channels_encoding(epochs, channels_out, losses, layers_type, data, noise, std=None, batch_size=None, generation=None, number_layers=None):
+    #first folder to get data and second to where to stock comparisons in config.py
     folder = c.results_folder
     folder_comparison = find_comparison_path(epochs,data,noise,std,batch_size,generation)
 
     os.mkdir(folder_comparison)
 
+    # dict to save loss in different files and not same graph bc not on the same scale
     list_dict_acc = []
     dict_dict_losses = {loss: [] for loss in losses}
     list_dict_trustworthiness = []
 
+    # to save only one accuracy because always the same
     second_folder_acc_init = find_second_folder_results_influence_of_encoding_dim_name(epochs, channels_out[0], losses[0], layers_type[0], data, generation, number_layers, batch_size, noise, std)
     path_acc_init = find_path(folder, second_folder_acc_init, 1)
-
     acc_init = load_dict(path_acc_init, c.results_accuracy_init + c.extension_dict)
     list_dict_acc.append((acc_init, "Initial accuracy without AE"))
 
+    # for each layers and each channels we want to put acc + trust + loss in the list/dict for the loss
     for layer in layers_type:
         for channel in channels_out:
             for loss in losses:
@@ -43,6 +47,7 @@ def compare_channels_encoding(epochs, channels_out, losses, layers_type, data, n
                 loss_value = load_dict(path, c.results_losses + c.extension_dict)
                 dict_dict_losses[loss].append((loss_value, f"Loss | channel : {channel} | layer : {layer} | loss : {loss}"))
 
+    # create images
     show_metrics_from_dicts(list_dict_acc, path=folder_comparison + "/acc", xlabel="Encoding dimension", ylabel="Accuracy", title="Accuracy in function of encoding dimension")
     show_metrics_from_dicts(list_dict_trustworthiness, path=folder_comparison + "/trustworthiness", xlabel="Encoding dimension", ylabel="Trustworthiness", title="Trustworthiness in function of encoding dimension")
     for loss in losses:
